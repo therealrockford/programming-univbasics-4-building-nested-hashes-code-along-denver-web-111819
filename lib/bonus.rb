aad9d703f542807f08a3263f45ce818ed8a60301
Rocky Pitto <rockypitto@gmail.com>
2019-10-18 20:18:04 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,7 +20,11 @@ def bonus
    }
   }
 
-  #code your solution here:
+epic_tragedy[:montague][:hero][:status] = "dead"
+
+epic_tragedy[:capulet][:heroine][:status] = "dead"
+
+puts epic_tragedy
 
   
 
